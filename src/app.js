1bb477cbc52457f4a9fe48ab813342e228d2ce40
Tushal <tushalhansola15@gmail.com>
2026-04-15 09:13:26 +0530

    change the route modify

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/students",studentRouter)
 
-app.get("/",(req,res)=>{
+app.get("/test",(req,res)=>{
     res.send("API is working fine");
 })
 

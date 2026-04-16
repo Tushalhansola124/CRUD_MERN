f94be542426cfbfc84d6b397f27d2063a94594da
Tushal <tushalhansola15@gmail.com>
2026-04-16 23:03:22 +0530

    this depencay add

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const studentRouter = require("./router/student.router");
 
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true
+}));
 app.use(express.json());
 app.use("/api/students",studentRouter)
 

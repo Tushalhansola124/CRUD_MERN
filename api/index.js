76cd0e943447f4c1264ca7e40f03a95231de1ced
Tushal <tushalhansola15@gmail.com>
2026-04-16 23:15:19 +0530

    this add cors file and change the script

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const app = require("../src/app");
+const app = require("../src/app")
 const connectDB = require("../src/db/db");
 
 connectDB();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const studentRouter = require("./router/student.router");
+const cors = require('cors');
 
 
 const app = express();
